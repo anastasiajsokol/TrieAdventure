22866223a34fa5efb41c0639710c9bed1663bdda
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-06-02 20:01:59 -0600

    really rough testing setup

---
+++ tests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "nodes/node.hpp"
+#include "trie.hpp"
+
+TEST(SystemTest, TrieSanityCheck) {
+    Trie<Node> trie;
+
+    trie.insert("hello");
+
+    EXPECT_TRUE(trie.contains("hello"));
+    EXPECT_FALSE(trie.contains("hell"));
+
+    EXPECT_TRUE(trie.is_prefix("hell"));
+}
