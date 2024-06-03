61f1cc903185c61a7377f3dcd7a0cff087ca6f97
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-06-02 20:08:25 -0600

    better organized (and extendable) testing

--- tests/system_tests.cpp
+++ tests/system_tests.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
-#include "nodes/node.hpp"
-#include "trie.hpp"
+#include "../nodes/node.hpp"
+#include "../trie.hpp"
 
 TEST(SystemTest, TrieSanityCheck) {
     Trie<Node> trie;
