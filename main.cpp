f343e470846370f778e484c3a48cceea647732fe
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-06-02 20:02:59 -0600

    now that there are tests, save main for other things

--- main.cpp
+++ main.cpp
@@ -6,12 +6,5 @@
 int main(){
 	std::cout << "You have successfully built Trie Adventure!\n";
 
-	Trie<Node> trie;
-
-	trie.insert("hello");
-
-	std::cout << "contains hello: " << trie.contains("hello") << "\ncontains hell: " << trie.contains("hell") << "\nprefix hell: " << trie.is_prefix("hell") << '\n';
-
 	return EXIT_SUCCESS;
 }
-
